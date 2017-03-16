a737405574629306d9e871cd16a2fb0323340c91
mrbcy <yang@ricearmy.com>
2017-03-16 10:46:01 +0800

    更新了Python调用其他程序的技术验证代码

---
+++ 技术验证代码/ExecPython/main.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+import os
+
+
+def func():
+    os.system('test.bat')
+
+
+if __name__ == '__main__':
+    func()
---
+++ 技术验证代码/ExecPython/test.bat
@@ -0,0 +1,2 @@
+cd /d "D:\软件编程学习\Mproxy\代码\Spiders\kuaidaili"
+scrapy crawl kuaidaili
